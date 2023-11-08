fcdcec1baf8e8c6e4f1d0b85a99d9f24853f41c6
Pedro2712 <pedroa3@al.insper.edu.br>
2023-11-08 20:03:36 -0300

    teste do botao

--- blink.cpp
+++ blink.cpp
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "pico/time.h"
 
 const uint LED_PIN = 28;
 const uint BUTTON_PIN = 14;
+const uint DEBOUNCE_DELAY_MS = 500;
 
 int main() {
     stdio_init_all();
 
+    // Configuração dos pinos
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
@@ -15,9 +18,14 @@ int main() {
     gpio_pull_down(BUTTON_PIN);
 
     printf("Start\n");
+
     while (true) {
+        // Leitura do botão com debounce
         if (gpio_get(BUTTON_PIN) == 1) {
+            // Inverte o estado do LED
             gpio_put(LED_PIN, !gpio_get(LED_PIN));
+            printf("Button pressed. LED state: %d\n", gpio_get(LED_PIN));
+            sleep_ms(DEBOUNCE_DELAY_MS);
         }
     }
 
