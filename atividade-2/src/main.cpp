d0dbc2c9784f682cc134b664c8e909a978e68fde
Pedro2712 <pedroa3@al.insper.edu.br>
2023-11-10 14:54:47 -0300

    teste da atividade 2

--- atividade-2/src/main.cpp
+++ atividade-2/src/main.cpp
@@ -11,36 +11,8 @@ const uint DEBOUNCE_DELAY_MS = 250;
 int main() {
     stdio_init_all();
 
-    // Configuração dos pinos
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_init(LED_PIN2);
-    gpio_set_dir(LED_PIN2, GPIO_OUT);
-
-    gpio_init(BUTTON_PIN);
-    gpio_set_dir(BUTTON_PIN, GPIO_IN);
-    gpio_pull_down(BUTTON_PIN);
-    gpio_init(BUTTON_PIN2);
-    gpio_set_dir(BUTTON_PIN2, GPIO_IN);
-    gpio_pull_down(BUTTON_PIN2);
-
-    printf("Start\n");
-
     while (true) {
-        // Leitura do botão com debounce
-        if (gpio_get(BUTTON_PIN) == 1) {
-            // Inverte o estado do LED
-            gpio_put(LED_PIN, !gpio_get(LED_PIN));
-            printf("Button press. LED state: %d\n", gpio_get(LED_PIN));
-            sleep_ms(DEBOUNCE_DELAY_MS);
-        }
-
-        if (gpio_get(BUTTON_PIN2) == 1) {
-            // Inverte o estado do LED
-            gpio_put(LED_PIN2, !gpio_get(LED_PIN2));
-            printf("Button press 2. LED state: %d\n", gpio_get(LED_PIN2));
-            sleep_ms(DEBOUNCE_DELAY_MS);
-        }
+        printf("passou");
     }
 
     return 0;
