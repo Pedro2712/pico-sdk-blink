a048426507ddc94ab92161b89a66d2d97587bd95
Pedro2712 <pedroa3@al.insper.edu.br>
2023-11-10 15:36:59 -0300

    teste da atividade 3

---
+++ atividade-3/src/main.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "pico/time.h"
+
+const uint LED_PIN = 28;
+const uint LED_PIN2 = 27;
+const uint BUTTON_PIN = 14;
+const uint BUTTON_PIN2 = 13;
+const uint DEBOUNCE_DELAY_MS = 250;
+
+int main() {
+    stdio_init_all();
+
+    while (true) {
+        sleep_ms(500);
+        printf("teste\n");
+    }
+
+    return 0;
+}
